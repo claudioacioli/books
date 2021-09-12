21a8747842390b945b294d60ca17f8dbdde6399f
Claudio Acioli <claudio.acioli.silva@gmail.com>
2021-09-12 13:48:03 -0300

    feat: Create method to insert data on database

--- src/main/java/com/bootcamp/books/dao/AuthorDAO.java
+++ src/main/java/com/bootcamp/books/dao/AuthorDAO.java
@@ -13,4 +13,19 @@ public class AuthorDAO {
     public AuthorDAO (Connection connection) {
         this.connection = connection;
     }
+
+    public void insert (Author author) {
+        try {
+            String dml = "insert into authors (name, email, resume, birthday) values (?, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(dml);
+            statement.setString(1, author.getName());
+            statement.setString(2, author.getEmail());
+            statement.setString(3, author.getResume());
+            statement.setDate(4, Date.valueOf(author.getBirthday()));
+            statement.execute();
+            statement.close();
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
 }
