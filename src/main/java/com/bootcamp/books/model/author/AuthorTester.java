d306a50581d4b5c5387567baf602ff7f94b0edec
Claudio Acioli <claudio.acioli.silva@gmail.com>
2021-09-12 11:44:20 -0300

    test: Test drive new constructor

--- src/main/java/com/bootcamp/books/model/author/AuthorTester.java
+++ src/main/java/com/bootcamp/books/model/author/AuthorTester.java
@@ -10,10 +10,28 @@ public class AuthorTester {
         author1.setEmail("calnewport@gmail.com");
         author1.setBirthday(LocalDate.parse("1982/06/23", DateTimeFormatter.ofPattern("yyyy/MM/dd")));
         author1.setResume(
-                author1.getName() + "(born " + author1.getBirthday().format(DateTimeFormatter.ofPattern("yyyy")) + ")" +
+                author1.getName() + " (born " + author1.getBirthday().format(DateTimeFormatter.ofPattern("yyyy")) + ")" +
                 " is an American non-fiction author and associate professor " +
                         "of computer science at Georgetown University.");
 
-        System.out.println(author1);
+        println(author1);
+
+        Author author2 = new Author(
+                "Dale Carnegie",
+                "dale_carnegie@gmail.com",
+                "Dale Carnegie (born 1988)  was an American writer and lecturer, " +
+                        "and the developer of courses in self-improvement, " +
+                        "salesmanship, corporate training, public speaking, and interpersonal skills.",
+                LocalDate.parse("11/24/1988", DateTimeFormatter.ofPattern("MM/dd/yyyy"))
+        );
+
+        println(author2.getName());
+        println(author2.getBirthday().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+        println(author2.getEmail());
+        println(author2.getResume());
+    }
+
+    private static <T> void println (T message) {
+        System.out.println(message);
     }
 }
