377285b7866ee2f954a77fa95908e0100a483e5c
Claudio Acioli <claudio.acioli.silva@gmail.com>
2021-09-12 13:32:59 -0300

    feat: Create factory to access database

---
+++ src/main/java/com/bootcamp/books/factory/ConnectionFactory.java
@@ -0,0 +1,33 @@
+package com.bootcamp.books.factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    public Connection getConnection () {
+        try {
+            return DriverManager.getConnection(getUri());
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private String getUri () {
+        int port = 3306;
+        String host = "10.0.3.156";
+        String user = "books";
+        String password = "books";
+        String database = "books";
+
+        return String.format(
+                "jdbc:mariadb://%s:%d/%s?user=%s&password=%s",
+                host,
+                port,
+                database,
+                user,
+                password
+        );
+    }
+}
